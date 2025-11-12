6cb775295a0f705d71bd7022638f9ad16bff8fa6
sarthakp972 <sarthakpatwa972@gmai.com>
2025-11-12 18:47:50 +0530

    make  new model

--- user_service/models.py
+++ user_service/models.py
@@ -29,6 +29,21 @@ class UserProfile(Base):
         return '{}-{}-{}'.format(self.id, self.email, self.user_type)
     
 
+class CompanyDetails(Base):
+    user = models.ForeignKey(
+        "UserProfile", on_delete=models.CASCADE, null=True, blank=True
+    )
+    company_name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=20, null=True, blank=True)
+    emirates_id = models.CharField(max_length=50, null=True, blank=True)
+    company_code = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.company_name
+
+
+
+
 class PropertyManagerCompanyDetails(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="property_manager_details")
     company_code = models.CharField(max_length=255)
