423fbe6cbdd49ed301a837e7217e47602deba602
Kishor <kishor@Doqfy.in>
2026-02-26 23:56:27 +0530

    removed unused code

--- terms/models.py
+++ terms/models.py
@@ -9,14 +9,3 @@ class TermsAndConditions(models.Model):
 
     def __str__(self):
         return f"{self.key} - {self.title}"
-'''from django.db import models
-
-
-class Terms(models.Model):
-    key = models.CharField(max_length=100)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    def __str__(self):
-        return self.title'''
