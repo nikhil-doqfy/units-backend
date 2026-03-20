8439d756edef3119722f506085e59738c58bb4c8
Nikhil Rane <nikhilrane1992@gmail.com>
2026-03-20 17:43:44 +0530

    Added changes

--- lease/models.py
+++ lease/models.py
@@ -95,6 +95,12 @@ class Lease(Base):
     contract_amount = models.FloatField(null=True, blank=True)
     payment_count = models.IntegerField(null=True, blank=True, help_text="Number of installments")
     shell_and_core = models.BooleanField(default=False, help_text="Is the property Shell?")
+    platform = models.CharField(
+        max_length=20,
+        choices=constants.PLATFORM_CHOICES,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return "{}-{}".format(self.code or self.id, self.lease_status)
